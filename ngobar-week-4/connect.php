bf9e95f93c873085f86aee2e59f4f4468989192c
ArtaHendraa <artahenda@gmail.com>
2024-11-10 10:39:36 +0800

    update

--- ngobar-week-4/connect.php
+++ ngobar-week-4/connect.php
@@ -7,14 +7,3 @@ $db_name = "ngobar_week_4";
 
 
 $conection = mysqli_connect($host, $user,$password, $db_name);
-
-
-$names = ["arta", "egi", "agus", "deni","rico", "jhon doe"];
-foreach($names as $name){
-  echo"hello $name \n";
-}
-// if($conection) {
-//   echo "aman";
-// } else {
-//   echo "error";
-// }
